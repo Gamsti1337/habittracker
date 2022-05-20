ef769cafd72881f0f74a0c180abbf7ba966ba4a2
Gamsti1337 <stecherul69420@protonmail.com>
2022-05-20 11:21:49 +0300

    done some buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from "react"
 import './styles.css'
 import 'boxicons'
-//onClick={window.location.href = "https://github.com/Gamsti1337/habittracker"}
+
 function Entries(props) {
   return (
     <div className="entry">
-      <box-icon name={props.emojiname} type={props.type}></box-icon>
+      <button id="transparentbutton">
+        <box-icon name={props.emojiname} type={props.type}></box-icon>
+      </button>
       <h3>{props.textcontent}</h3>
     </div>
-  );
+  )
 }
 
 function App() {
@@ -17,14 +19,14 @@ function App() {
       <Entries textcontent="Investing" emojiname="money-withdraw"/>
       <Entries textcontent="Journaling" emojiname="book-alt"/>
       <Entries textcontent="Learning" emojiname="graduation" type="solid"/>
-      <Entries textcontent="" emojiname=""/>
       <div className="footer">
-        <button>
+        <button onClick={event => window.location.href = "https://github.com/Gamsti1337"} id="transparentbutton">
+          <abbr title="Visit my GitHub page so you can see this projects source code and more projects of mine"></abbr>
           <box-icon name='github' type='logo' ></box-icon>
         </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
